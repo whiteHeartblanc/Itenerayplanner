07adebc1d367477648a4695c3361ba2b32a6ba17
whiteHeartblanc <45506990+whiteHeartblanc@users.noreply.github.com>
2019-08-21 20:03:57 +0800

    port

--- server.js
+++ server.js
@@ -497,9 +497,8 @@ app.get("/back", function(req,res){
     
 })
 
-
-
-app.listen(3000, function(){
+var port = process.env.PORT|| 3000
+app.listen(port, function(){
     
     console.log("live at port 3000")
 })
