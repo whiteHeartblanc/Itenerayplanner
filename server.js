4a7f9bb6dabbc493f4214521009eca670cbe77d7
whiteHeartblanc <45506990+whiteHeartblanc@users.noreply.github.com>
2019-08-22 09:12:30 +0800

    new /

--- server.js
+++ server.js
@@ -39,8 +39,14 @@ app.use(express.static(__dirname+"/public"))
 
 
 app.get("/", function(req,res){
-
-  let itemlist;
+res.redirect("/base")
+ 
+    
+})
+app.get("base", function(req,res){
+    
+    
+     let itemlist;
     let listname;
     let date;
     console.log(req.cookies.curList)
